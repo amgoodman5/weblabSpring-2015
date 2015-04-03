24c41a4d85bd00890d7cdad43abb1530b6cb7ff1
Aaron Goodman <Amgoodman5@gmail.com>
2015-04-02 20:07:06 -0600

    <Updates>

---
+++ html_week_1/Intro to programming/Highlow/DIV/Div project 4/1/index.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    $(function(){
+            $('.box').click(function(){
+            $(this).addClass('blue');
+            console.log($(this).attr('id')
+            );
+                .mouseOut(function()
+        if($(this).hasClass('blue')) {
+            $(this).removeClass('blue');
+        };
+        alert('bye!');
+        $(this).removeClass('blue');
+
+        $('button').click(function(){
+            $(this).append('Hello')
+        })                    })
+});
+})
+
---
+++ html_week_1/restaurantMenu/index.js
@@ -0,0 +1,25 @@
+
+//$(function(){
+    var box = $('#specials,#LunchSpecials');
+    box.on('mouseenter', function(){
+        box
+            .stop(true)
+            .animate({
+                backgroundColor: 'green'
+                , width: 862
+                , height: 500
+            }, 1500);
+    });
+    box.on('mouseleave', function(){
+        box
+            .stop(true)
+            .animate({
+                backgroundColor: 'red'
+                , width:  139
+                , height: 200
+            }, 1500);
+    });
+});
+
+
+
