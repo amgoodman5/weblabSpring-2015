081e2b72a4a9d180214bd188f47bd746c0d8a2ae
Aaron Goodman <Amgoodman5@gmail.com>
2015-04-07 11:32:27 -0600

    <c.ss updates>

---
+++ html_week_2/Stop light/index.js
@@ -0,0 +1,12 @@
+                 $(function() {
+    			$('button').on('click',function() {
+        				$('.bulb').removeClass('on');
+        				$('button').removeClass('on');
+
+            				var color = $(this).attr('data-color');
+        				$("." + color).toggleClass('on');
+        				$(this).addClass('on');
+
+
+                			});
+    		});
